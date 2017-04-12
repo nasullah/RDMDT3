a4e38792e3f1801fb1c605180df6a395f0ac9dec
Nasullah Alham <nasullah.khalidalham@ndcls.ox.ac.uk>
2017-04-12 15:52:23 +0100

    Removed the upload function

--- grails-app/controllers/rdmdt/DiseaseGroupsController.groovy
+++ grails-app/controllers/rdmdt/DiseaseGroupsController.groovy
@@ -25,46 +25,40 @@ class DiseaseGroupsController {
     }
 
     def assetResourceLocator
-    @Transactional
-    def test(){
-//        def jsonObject = new JsonSlurper().parseText('{"DiseaseGroups": [ {"id": "10950","name": "Cardiovascular disorders","subGroups": [ {"id": "33332","name": "Arteriopathies", "specificDisorders": [{"id": "33666","name": "Familial Hypercholesterolaemia","eligibilityQuestion": {"date": "2016-06-28","version": "5"},"shallowPhenotypes": [{"name": "Hyperlipidemia","id": "HP:0003077"},{"name": "Hypercholesterolemia","id": "HP:0003124"},{"name": "Hyperbetalipoproteinemia","id": "HP:0003141"},{"name": "Hyperlipoproteinemia","id": "HP:0010980"},{"name": "Myocardial infarction","id": "HP:0001658"},{"name": "Hypertriglyceridemia","id": "HP:0002155"},{"name": "Tendon xanthomatosis","id": "HP:0010874"},{"name": "Corneal arcus","id": "HP:0001084"}],"tests": [{"name": "Lipids","id": "33158.1"}]}] }] }] }')
-        def inputFile = new File("grails-app/assets/json/DiseaseOntology-v1.5.2.json")
-        def jsonObject = new JsonSlurper().parseText(inputFile.text)
-
-        jsonObject.DiseaseGroups.each{dg ->
-            def diseaseGroupsInstance = new DiseaseGroups(originalId: dg.id, name: dg.name)
-            dg.subGroups.each{sg ->
-                def subGroupsInstance = new SubGroups(originalId: sg.id, name: sg.name)
-                diseaseGroupsInstance.addToSubGroups(subGroupsInstance).save()
-                sg.specificDisorders.each{sd ->
-                    def specificDisordersInstance = new SpecificDisorders(originalId: sd.id, name: sd.name)
-                    subGroupsInstance.addToSpecificDisorders(specificDisordersInstance).save()
-                    print(sd.eligibilityQuestion.version)
-                    print(sd.eligibilityQuestion.date)
-                    specificDisordersInstance.eligibilityQuestion = new EligibilityQuestion(date: new Date().parse("yyyy-MM-dd", sd.eligibilityQuestion.date), versionNumber: sd.eligibilityQuestion.version)
-                    specificDisordersInstance.save()
-                    sd.shallowPhenotypes.each{sp ->
-                        def shallowPhenotypesInstance = new ShallowPhenotypes(originalId: sp.id, name: sp.name)
-                        specificDisordersInstance.addToShallowPhenotypes(shallowPhenotypesInstance).save()
-                    }
-                    sd.tests.each{test ->
-                        def testsInstance = new Tests(originalId: test.id, name: test.name)
-                        specificDisordersInstance.addToTests(testsInstance).save()
-                    }
-                }
-            }
-        }
-
-        redirect action: "index", method: "GET"
-
-    }
-
-    def testfile(){
-        def inputFile = ApplicationHolder.getApplication().getParentContext().getResource("/DiseaseOntology-v1.5.2.json").getFile()
-        def InputJSON = new JsonSlurper().parseText(inputFile.text)
-        InputJSON.each{ println it }
-        redirect action: "index", method: "GET"
-    }
+//    @Transactional
+//    def test(){
+////        def jsonObject = new JsonSlurper().parseText('{"DiseaseGroups": [ {"id": "10950","name": "Cardiovascular disorders","subGroups": [ {"id": "33332","name": "Arteriopathies", "specificDisorders": [{"id": "33666","name": "Familial Hypercholesterolaemia","eligibilityQuestion": {"date": "2016-06-28","version": "5"},"shallowPhenotypes": [{"name": "Hyperlipidemia","id": "HP:0003077"},{"name": "Hypercholesterolemia","id": "HP:0003124"},{"name": "Hyperbetalipoproteinemia","id": "HP:0003141"},{"name": "Hyperlipoproteinemia","id": "HP:0010980"},{"name": "Myocardial infarction","id": "HP:0001658"},{"name": "Hypertriglyceridemia","id": "HP:0002155"},{"name": "Tendon xanthomatosis","id": "HP:0010874"},{"name": "Corneal arcus","id": "HP:0001084"}],"tests": [{"name": "Lipids","id": "33158.1"}]}] }] }] }')
+//        def path = grailsApplication.config.uploadFolder + "DiseaseOntology-v1.6.0.json"
+//        def inputFile = new File(path)
+//        def jsonObject = new JsonSlurper().parseText(inputFile.text)
+//
+//        jsonObject.DiseaseGroups.each{dg ->
+//            def diseaseGroupsInstance = new DiseaseGroups(originalId: dg.id, name: dg.name)
+//            dg.subGroups.each{sg ->
+//                def subGroupsInstance = new SubGroups(originalId: sg.id, name: sg.name)
+//                diseaseGroupsInstance.addToSubGroups(subGroupsInstance).save()
+//                sg.specificDisorders.each{sd ->
+//                    def specificDisordersInstance = new SpecificDisorders(originalId: sd.id, name: sd.name)
+//                    subGroupsInstance.addToSpecificDisorders(specificDisordersInstance).save()
+//                    print(sd.eligibilityQuestion.version)
+//                    print(sd.eligibilityQuestion.date)
+//                    specificDisordersInstance.eligibilityQuestion = new EligibilityQuestion(date: new Date().parse("yyyy-MM-dd", sd.eligibilityQuestion.date), versionNumber: sd.eligibilityQuestion.version)
+//                    specificDisordersInstance.save()
+//                    sd.shallowPhenotypes.each{sp ->
+//                        def shallowPhenotypesInstance = new ShallowPhenotypes(originalId: sp.id, name: sp.name)
+//                        specificDisordersInstance.addToShallowPhenotypes(shallowPhenotypesInstance).save()
+//                    }
+//                    sd.tests.each{test ->
+//                        def testsInstance = new Tests(originalId: test.id, name: test.name)
+//                        specificDisordersInstance.addToTests(testsInstance).save()
+//                    }
+//                }
+//            }
+//        }
+//
+//        redirect action: "index", method: "GET"
+//
+//    }
 
     @Transactional
     def save(DiseaseGroups diseaseGroupsInstance) {
